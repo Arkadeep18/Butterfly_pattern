32e3ef1c961917efe82884a513a9a3a49a323af4
Arka <146548597+Arkadeep18@users.noreply.github.com>
2024-01-02 00:46:53 +0530

    prac37.java

--- Prac37.java
+++ Prac37.java
@@ -3,40 +3,45 @@ public class Prac37 {
 
 	public static void main(String[] args) {
 		int n = 5;
+		
+		//Butterfly Pattern
+			 
+			//First half's star
 		for(int a = 1;a<=n;a++) {
 			for(int b = 1;b<=a;b++) {
 				System.out.print("*");
 			}
-					//Space Print
+			//Space Print
 			int space = 2*(n-a);
 			for(int b = 1; b<=space; b++) {
 				System.out.print(" ");
 			}
+			//Second half's star
 			for(int b = 1; b<=a; b++) {
-				System.out.print("*");  //Star print
+				System.out.print("*");
 			}
 			System.out.println();
 		
 		}
 					//Second Part
+		
+				//first half's star
 			for(int a = n; a>=1;a--) {
 				for(int b = 1; b<=a; b++) {
-					System.out.print("*"); //first star print
+					System.out.print("*"); 
 				}
+				//Space print
 				int space = 2*(n-a);
 				for(int b = 1; b<=space; b++) {
 					System.out.print(" ");
 				}
+				//Second half's star
 				for(int b = 1; b<=a; b++) {
 					System.out.print("*");
 				}
-				System.out.println(
-);
-			}
+				System.out.println();
+		}
 			
-				
-	
 	}
+	
 }
-
-
